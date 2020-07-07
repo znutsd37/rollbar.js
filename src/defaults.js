1aef6ccf838623d84ca1f1f71a9d99b9e45377c9
Walt Jones <waltjones@gmail.com>
2020-07-07 16:43:23 -0700

    avoid loading all of package.json in componentized builds

---
+++ src/defaults.js
@@ -0,0 +1,9 @@
+module.exports = {
+  version: '2.14.6',
+  endpoint: 'api.rollbar.com/api/1/item/',
+  logLevel: 'debug',
+  reportLevel: 'debug',
+  uncaughtErrorLevel: 'error',
+  maxItems: 0,
+  itemsPerMin: 60
+}
